3226dbf0370473008fa03dbca356e1df2b95800a
NatashaEarley <nearley@unca.edu>
2024-11-26 22:22:22 -0500

    HW4

--- homework/hw04/src/components/NavBar.jsx
+++ homework/hw04/src/components/NavBar.jsx
@@ -1,30 +1,7 @@
 import React from "react";
-import { getDataFromServer } from "../server-requests";
 
 export default function NavBar({ username }) {
-    const [navBar, setNavBar] = useState([]);
-
-    async function getNavBar() {
-        const data = await getDataFromServer(token, "/api/profile");
-        setNavBar(data);
-    
-    }
-    useEffect(() => {
-        getNavBar();
-    }, []);
-
-    function outputNavBar(profileObj) {
-        return <NavBar token={token} key={profileObj.id} postData={profileObj} />
-    }
-
-    return (
-        <div>
-            {
-                navBar.map(outputNavBar)
-            }
-        </div>
-        );
-
+    // This component is implemented for you:
     return (
         <nav className="flex justify-between py-5 px-9 bg-white border-b fixed w-full top-0">
             <h1 className="font-Comfortaa font-bold text-2xl">Photo App</h1>
--- homework/hw04/src/components/Profile.jsx
+++ homework/hw04/src/components/Profile.jsx
@@ -1,30 +1,6 @@
 import React from "react";
-import { getDataFromServer } from "../server-requests";
 
-export default function Profile({ profile, token }) {
-    const [profiles, setProfile] = useState([]);
-
-    async function getProfile() {
-        const data = await getDataFromServer(token, "/api/profile");
-        setProfile(data);
-    
-    }
-    useEffect(() => {
-        getProfile();
-    }, []);
-
-    function outputProfile(profileObj) {
-        return <Story token={token} key={profileObj.id} postData={profileObj} />
-    }
-
-    return (
-        <div>
-            {
-                profiles.map(outputProfile)
-            }
-        </div>
-        );
-}
+export default function Profile({ token }) {
 
     return (
         <header className="flex gap-4 items-center">
