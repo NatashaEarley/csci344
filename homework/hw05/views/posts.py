aedd64cb8a0619baa9c93a5a782b0056ce54f851
NatashaEarley <nearley@unca.edu>
2024-12-06 23:56:01 -0500

    Starting HW5

--- homework/hw05/views/posts.py
+++ homework/hw05/views/posts.py
@@ -5,7 +5,7 @@ from flask_restful import Resource
 
 from models import db
 from models.post import Post
-from views import get_authorized_user_ids
+from views import get_authorized_user_ids, can_view_post
 
 
 def get_path():
@@ -21,7 +21,14 @@ class PostListEndpoint(Resource):
 
         # giving you the beginnings of this code (as this one is a little tricky for beginners):
         ids_for_me_and_my_friends = get_authorized_user_ids(self.current_user)
-        posts = Post.query.filter(Post.user_id.in_(ids_for_me_and_my_friends))
+        try:    
+            count = int(request.args.get("limit", 20))
+            if count > 50:
+                count = 50
+        except:
+            count = 20
+        
+        posts = Post.query.filter(Post.user_id.in_(ids_for_me_and_my_friends)).limit(count)
 
         # TODO: add the ability to handle the "limit" query parameter:
 
@@ -29,8 +36,29 @@ class PostListEndpoint(Resource):
         return Response(json.dumps(data), mimetype="application/json", status=200)
 
     def post(self):
-        # TODO: handle POST logic
-        return Response(json.dumps({}), mimetype="application/json", status=201)
+       data = request.json
+       print(data)
+       image_url = data.get("image_url")
+       caption = data.get("caption")
+       alt_text = data.get("alt_text")
+
+       if not image_url:
+           return Response(
+                json.dumps({"message": "image_url is a required parameter"}),
+                minetype="application/json",
+                status=400,
+           )
+
+       new_post = Post(
+            image_url=image_url,
+            user_id=self.current_user.id,
+            caption=caption,
+            alt_text=alt_text,
+        )
+       db.session.add(new_post)
+       db.session.commit() 
+       
+       return Response(json.dumps(new_post.to_dict(user=self.current_user)), mimetype="application/json", status=201)
 
 
 class PostDetailEndpoint(Resource):
@@ -55,12 +83,21 @@ class PostDetailEndpoint(Resource):
 
     def get(self, id):
         print("POST id=", id)
-        # TODO: Add GET logic...
-        return Response(
-            json.dumps({}),
-            mimetype="application/json",
-            status=200,
-        )
+        can_view = can_view_post(id, self.current_user)
+        if (can_view):
+            post = Post.query.get(id)
+            return Response(
+                json.dumps(post.to_dict(user=self.current_user)),
+                mimetype="application/json",
+                status=200,
+            )
+        else:
+             return Response(
+                json.dumps({"Message": f"Post id={id} not found"}),
+                mimetype="application/json",
+                status=404,
+            )
+
 
 
 def initialize_routes(api, current_user):
